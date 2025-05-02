4d845cb1cac96cb06bd0e70ec345035184ed6a8c
Mohmmad Khaseeb <Mkhaseeb84@gmail.com>
2025-05-02 19:44:43 +0300

    Login&registration(Front-end)

--- allim/allimApp/views.py
+++ allim/allimApp/views.py
@@ -4,4 +4,9 @@ def index(request):
     return render(request,'index.html')
 
 
+def Login_page(request):
+    return render(request,'login.html')
+
+def register_page(request):
+    return render(request,'registration.html')
 # Create your views here.
