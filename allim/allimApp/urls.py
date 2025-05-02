f09e0ed4092dfbd69265ffd7b6b75a9ae8a4f34d
Ibrahemiyad <165409542+Ibrahemiyad@users.noreply.github.com>
2025-05-02 15:40:44 +0300

    teache_dashboard
    teache_dashboard

--- allim/allimApp/urls.py
+++ allim/allimApp/urls.py
@@ -5,4 +5,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index ,name ='index'),  # Root path
-    ]
+    path('register', views.register_user, name='register'),
+    path('teacher/dashboard/', views.teacher_dashboard, name='teacher_dashboard'),
+    path('student/dashboard/', views.student_dashboard, name='student_dashboard'),
+]
