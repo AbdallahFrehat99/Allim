4e6a13e83e975b12e3c3f29cbbbf693e5441fa16
Mohmmad Khaseeb <Mkhaseeb84@gmail.com>
2025-05-02 20:14:00 +0300

    views conflict

--- allim/allimApp/views.py
+++ allim/allimApp/views.py
@@ -2,11 +2,3 @@ from django.shortcuts import render
 
 def index(request):
     return render(request,'index.html')
-
-
-def Login_page(request):
-    return render(request,'login.html')
-
-def register_page(request):
-    return render(request,'registration.html')
-# Create your views here.
